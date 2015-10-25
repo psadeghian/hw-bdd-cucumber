b8956e20f2b2f4753eea9958471a381d99c9725e
psadeghian <pasha1129@gmail.com>
2015-10-25 13:12:43 +0000

    Part 1 completed (I think)

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie) # PDev
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,6 +15,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
+  # PDev note: /./m - Any character (the m modifier enables multiline mode)
+  # PDev this is unrelated to Part 1 but it was quick coding, for now let's comment out
+  # page.body.should match /#{e1}.*#{e2}/m 
   fail "Unimplemented"
 end
 
